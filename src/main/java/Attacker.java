59e6a9deb455b19cde3eafe4746d7f8733b92e34
unknown <matias.giorgio@gmail.com>
2015-06-01 20:56:49 -0700

    toString() redefined in Attacker and Defender.

--- src/main/java/Attacker.java
+++ src/main/java/Attacker.java
@@ -4,4 +4,9 @@ public class Attacker extends AbstractFighter {
 	public Attacker() {
 		super(getRandom(50) + 49);
 	}
+	
+	@Override
+	public String toString() {
+		return "Attacker";
+	}
 }
--- src/main/java/Defender.java
+++ src/main/java/Defender.java
@@ -1,8 +1,11 @@
-
 public class Defender extends AbstractFighter {
 
 	public Defender() {
-		super(getRandom(49)+1);
+		super(getRandom(49) + 1);
 	}
 
+	@Override
+	public String toString() {
+		return "Defender";
+	}
 }
